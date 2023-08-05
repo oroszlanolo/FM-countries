a649bb4a77120d4796715e896379a2f7e7567edd
oroszlanolo <balazsi.balint1111@gmail.com>
2023-08-05 12:14:35 +0200

    fixed home button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ModeToggleService } from './mode-toggle/mode-toggle.service';
 import { Mode } from './mode-toggle/mode-toggle.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,14 @@ import { Mode } from './mode-toggle/mode-toggle.model';
 export class AppComponent {
   currentMode = this.modeToggleService.currentMode;
   Mode = Mode;
-  constructor(private modeToggleService: ModeToggleService) {}
+  constructor(private router: Router, private modeToggleService: ModeToggleService) {}
 
   toggleMode() {
     this.modeToggleService.toggleMode();
     this.currentMode = this.modeToggleService.currentMode;
+  }
+
+  goHome() {
+    this.router.navigate(['/home']);
   }
 }
